25e427472905b4cde64e480c9fd7df9bd78cf9f3
tlc2880 <tommylcao@gmail.com>
2020-01-02 21:33:35 -0800

    Add new todos feature.

--- app.js
+++ app.js
@@ -10,17 +10,68 @@ const mongoose = require('mongoose');
 app.use(express.static(__dirname+'/client'));
 app.use(bodyParser.json());
 
-Genre =require('./models/genre');
-Book =require('./models/book');
+Genre = require('./models/genre');
+Book = require('./models/book');
+Todo = require('./models/todo');
 
 // Connect to Mongoose
-mongoose.connect('mongodb://localhost/bookstore');
+mongoose.connect('mongodb://localhost/todosMEAN');
 var db = mongoose.connection;
 
 app.get('/', (req, res) => {
-	res.send('Please use /api/books or /api/genres');
+	res.send('Please use /api/books or /api/todos');
 });
 
+app.get('/api/todos', (req, res) => {
+	Todo.getTodos((err, todos) => {
+		if(err){
+			throw err;
+		}
+		res.json(todos);
+	});
+});
+
+app.post('/api/todos', (req, res) => {
+	var todo = req.body;
+	Todo.addTodo(todo, (err, todo) => {
+		if(err){
+			throw err;
+		}
+		res.json(todo);
+	});
+});
+
+app.put('/api/todos/:_id', (req, res) => {
+	var id = req.params._id;
+	var todo = req.body;
+	Todo.updateTodo(id, todo, {}, (err, todo) => {
+		if(err){
+			throw err;
+		}
+		res.json(todo);
+	});
+});
+
+// Add this to do edit genre
+app.get('/api/todos/:_id', (req, res) => {
+	Todo.getTodoById(req.params._id, (err, todo) => {
+		if(err){
+			throw err;
+		}
+		res.json(todo);
+	});
+});
+
+app.delete('/api/todos/:_id', (req, res) => {
+	var id = req.params._id;
+	Todo.removeTodo(id, (err, todo) => {
+		if(err){
+			throw err;
+		}
+		res.json(todo);
+	});
+});
+////////////////////////////////////////////////////////
 app.get('/api/genres', (req, res) => {
 	Genre.getGenres((err, genres) => {
 		if(err){
@@ -70,7 +121,7 @@ app.delete('/api/genres/:_id', (req, res) => {
 		res.json(genre);
 	});
 });
-
+/////////////////////////////////////////////////////////////////////////
 app.get('/api/books', (req, res) => {
 	Book.getBooks((err, books) => {
 		if(err){
